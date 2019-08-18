414e36f22cfc0293121b1bc779a038355b153da4
tianmaoyu@foxmail.com <tianmaoyu@foxmail.com>
2019-08-19 01:37:39 +0800

    test mvc controller

--- src/test/java/com/example/demo/apicontroller/UserControllerTest.java
+++ src/test/java/com/example/demo/apicontroller/UserControllerTest.java
@@ -1,13 +1,30 @@
 package com.example.demo.apicontroller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
+    @Autowired
+    private MockMvc web;
+
     @Test
-    public void getUser() {
+    public void getUser() throws Exception {
+        web.perform(MockMvcRequestBuilders.get("/api/user/getUser")).andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 
     @Test
