478679751b6a58dd337a12bf8b2c532998bacd8a
eric/田茂宇 <tianmaoyu@foxmail.com>
2019-08-28 15:55:34 +0800

    打包成不同的jar名

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.config.UserConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class HomeController {
 
 d
  */
+@Cacheable
     @RequestMapping("index")
     public  String index(ModelMap modelMap){
         modelMap.addAttribute("user",user);
